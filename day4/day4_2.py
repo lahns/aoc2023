753b351838e62926e0fd280cdff0cf696b0ddc2d
Wojciech Umiński <81372963+lahns@users.noreply.github.com>
2023-12-04 20:09:00 +0100

    more typos

--- day4/day4_2.py
+++ day4/day4_2.py
@@ -9,10 +9,10 @@ with open('input.txt', 'r') as file:
         if index not in scratches:
             scratches[index] = 1
         line = line.split(':')[1]
-        winning_numbers = line.strip().split('|')[0].split()
+        wining_numbers = line.strip().split('|')[0].split()
         game_numbers = line.strip().split('|')[1].split()
         
-        matching_numbers = sum(q in winning_numbers for q in game_numbers)
+        matching_numbers = sum(q in wining_numbers for q in game_numbers)
 
         for next in range(index + 1, index + matching_numbers + 1):
             scratches[next] = scratches.get(next, 1) + scratches[index]
